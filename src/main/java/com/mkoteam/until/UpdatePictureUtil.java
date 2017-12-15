e5a51db230b885fdabe8b3a95b955116efa0d890
songjuguang <sjg678827@163.com>
2017-12-15 17:14:00 +0800

    七个接口完成

---
+++ src/main/java/com/mkoteam/until/UpdatePictureUtil.java
@@ -0,0 +1,35 @@
+package com.mkoteam.until;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+/**
+ * 使用java调用命令行操作并执行结果
+ **/
+public class UpdatePictureUtil {
+
+    public static void exeCmd(String commandStr) {
+        BufferedReader br = null;
+        try {
+            Process p = Runtime.getRuntime().exec(commandStr);
+            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+//            System.out.println(sb.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
